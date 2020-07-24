3d153ab756d2006a40900b9fe0a6d5784c8c1e44
jainayu <500060884@stu.upes.ac.in>
2020-07-25 04:15:58 +0530

    setup for heroku

---
+++ src/app.js
@@ -0,0 +1,94 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsDirectoryPath = path.join(__dirname, '../templates/views')
+const partialsDirectoryPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engin and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsDirectoryPath)
+hbs.registerPartials(partialsDirectoryPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ayush Jain',
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Ayush Jain',
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: "I'll provide content soon",
+        name: 'Ayush Jain'
+    })
+})
+
+app.get('/weather', (req, res) =>{
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please provide an address'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        
+        if(error){
+            return res.send({ error })
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+
+            if(error){
+                return res.send({ error })
+            }
+            res.send({
+                location, 
+                forecast: forecastData,
+                address: req.query.address,
+
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) =>{
+    res.render('404', {
+        title: '404',title: '404',
+        message: "Help article not found",
+        name: 'Ayush Jain'
+    })
+})
+
+app.get('*', (req, res) =>{
+    
+    res.render('404', {
+        title: '404',
+        message: "Page not found",
+        name: 'Ayush Jain'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
